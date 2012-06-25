9a93f928e716d5886cd048eb4c2412feb275055c
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2012-06-25 00:51:19 -0300

    ajust

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Auction::Application.routes.draw do
 
   root :to => 'site#index'
-  match 'auth/facebook/callback', :to => 'session#create'
+  match 'auth/facebook/callback', :to => 'sessions#create'
   match 'auth/failure', :to => redirect('/')
 end
