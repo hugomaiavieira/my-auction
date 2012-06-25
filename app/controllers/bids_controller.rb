9794502daeab2ab270dfb632fd9c1a21e0f907bb
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2012-06-25 01:36:57 -0300

    Add bids index

---
+++ app/controllers/bids_controller.rb
@@ -0,0 +1,5 @@
+class BidsController < ApplicationController
+  def index
+    @bids = Bid.all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Auction::Application.routes.draw do
 
   root :to => 'site#index'
+  get 'bids', :to => 'bids#index'
   match 'auth/facebook/callback', :to => 'sessions#create'
   match 'auth/failure', :to => redirect('/')
 end
